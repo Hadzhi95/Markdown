d9e79de90ae039739be87269d26940bcc9460af5
Hadzhi Yakhyaev <kreyg1992@bk.ru>
2023-01-29 13:52:04 +0300

    noteList

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,8 +3,9 @@ import { useMemo } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
 import { useLocalStorage } from "./useLocalStorage"
-import {v4 as uuidV4} from "uuid"
+import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./NoteList"
+import { NoteLayout } from "./NoteLayout"
 
 export type Note = {
   id: string
@@ -31,18 +32,18 @@ export type Tag = {
 }
 
 function App() {
-  const [notes, setNotes] = useLocalStorage<RawNoteData[]>("NOTES", [])
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
-  const noteWithTags = useMemo(() => {
+  const notesWithTags = useMemo(() => {
     return notes.map(note => {
-      return {... note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
     })
   }, [notes, tags])
 
-  function onCreateNote({tags, ...data}: NoteData) {
+  function onCreateNote({ tags, ...data }: NoteData) {
     setNotes(prevNotes => {
-      return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
     })
   }
 
@@ -52,12 +53,12 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<NoteList/>} />
-      <Route path="/new" element={<NewNote 
-      onSubmit={onCreateNote} 
-      onAddTag={addTag}
-      availableTags={tags}/>} />
-      <Route path="/:id">
+      <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} />} />
+      <Route path="/new" element={<NewNote
+        onSubmit={onCreateNote}
+        onAddTag={addTag}
+        availableTags={tags} />} />
+      <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
         <Route index element={<h1>Show</h1>} />
         <Route path="edit" element={<h1>Edit</h1>} />
       </Route>
